04f1f47ac6ed645325fc5900a912e9ba0ef3eec5
christian.sonic <yoh.chrisitan@gmail.com>
2023-05-04 20:44:48 -0400

    Initial Commit

---
+++ Custom_CarouselSlider/Custom_CarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_CarouselSliderApp.swift
+//  Custom_CarouselSlider
+//
+//  Created by Sonic on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Custom_CarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
